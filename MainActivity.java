b6f66b801069ff75b606914cc74806a8d5359312
Nayana Tara <148714083+nayana1607tara@users.noreply.github.com>
2023-10-25 20:25:59 +0530

    Update MainActivity.java
    Added few functions

--- MainActivity.java
+++ MainActivity.java
@@ -6,10 +6,43 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView workingsTV;
+    TextView resultsTV;
+    String workings = "";
+    String formula = "";
+    String tempFormula = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextView();
+    }
+    private void initTextView() {
+        workingsTV = (TextView) findViewbyId(R.id.workingsTextView);
+        workingsTV = (TextView) findViewbyId(R.id.resultTextView);
+    }
+    private void setWorkings(String givenValue) {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
+    }
+
+    public void equaltoOnclick(view view) {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        checkForPowerof();
+
+        try{
+            result = (double) engine.eval(formula);
+        }
+        catch (ScriptException e) {
+            Toast.makeText(context: this, text: "Invalid Input", Toast.LENGTH_SHORT).show();
+
+        }
+        if (result != null){
+            resultsTV.setText(String.valueOf(result.doubleValue()));
+        }
     }
 
     private void CheckForPowerOf() {
@@ -37,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             } else
                 break;
         }
-        for (int i = index + 1; i > 0; i++) {
+        for (int i = index - 1; i > 0; i--) {
             if (isNumeric(workings.charAt(i)) == true) {
                 numberLeft = workings.charAt(i) + numberLeft;
             } else
@@ -53,6 +86,84 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-}
+    public void clearOnclick(view view) {
+        workingsTV.setText("");
+        workings = "";
+        resultsTV.setText("");
+        leftBracket = true;
+    }
 
+    boolean leftBracket = true;
+
+    public void bracketOnclick(view view) {
+        if(leftBracket == true) {
+            setworkings("(");
+            leftBracket = false;
+        }
+        else {
+            setWorkings(")");
+            leftBracket = true;
+        }
+    }
+    public void addOnclick(View view){
+        setWorkings("+");
+    }
+
+    public void subOnclick(View view){
+        setWorkings("-");
+    }
+
+    public void PowerOfOnclick(View view){
+        setWorkings("^");
+    }
+
+    public void mulOnclick(View view){
+        setWorkings("*");
+    }
+
+    public void divOnclick(View view){
+        setWorkings("/");
+    }
+
+    public void oneOnclick(View view){
+        setWorkings("1");
+    }
+
+    public void twoOnclick(View view){
+        setWorkings("2");
+    }
+    public void threeOnclick(View view){
+        setWorkings("3");
+    }
+
+    public void fourOnclick(View view){
+        setWorkings("4");
+    }
+
+    public void fiveOnclick(View view){
+        setWorkings("5");
+    }
+
+    public void sixOnclick(View view){
+        setWorkings("6");
+    }
+
+    public void sevenOnclick(View view){
+        setWorkings("7");
+    }
+
+    public void eightOnclick(View view){
+        setWorkings("8");
+    }
+
+    public void nineOnclick(View view){
+        setWorkings("9");
+    }
+
+    public void zeroOnclick(View view){
+        setWorkings("0");
+    }
+
+
+}
 //NayanaTara
